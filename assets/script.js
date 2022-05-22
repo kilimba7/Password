ba2a2a079802d11d415fbdb16e9cb86a310b8d79
Kilimba <dyaul003@umn.edu>
2022-05-22 12:14:04 -0500

    updated js

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,18 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min) + min);
+  
+    return value;
+  };
+  
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+console.log(Math)
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
